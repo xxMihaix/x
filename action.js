e482d2ad6bfffd2fb1b148a7d91f660ae71e21b6
xxMihaix <marius777bogdan@gmail.com>
2025-04-15 21:38:03 +0300

    Add files via upload

---
+++ action.js
@@ -0,0 +1,66 @@
+let matchResult = document.getElementById("matchresult");
+let counterSay = document.getElementById("counter");
+const rockSelected = document.getElementById("button1");
+const paperSelected = document.getElementById("button2");
+const scissorSelected = document.getElementById("button3");
+const deleteScore = document.getElementById("delete");
+
+let playerPick = ""; // Trebuie sa fie let
+
+counter = {
+    wins: 0,
+    loses: 0,
+    ties: 0 
+}
+
+function playGame(playerChoice) {
+    playerPick = playerChoice;
+
+    // Alegerea computerului
+    const computerPick = Math.floor(Math.random() * 3) + 1;
+    let computerPickSays = "";
+
+    if (computerPick === 1) {
+        computerPickSays = "Rock";
+    } else if (computerPick === 2) {
+        computerPickSays = "Paper";
+    } else {
+        computerPickSays = "Scissors";
+    }
+
+    // Determinare rezultat
+    let result = "";
+    if (playerPick === computerPickSays) {
+        result = "It's a tie!";
+        counter.ties += 1;
+    } else if (
+        (playerPick === "Rock" && computerPickSays === "Scissors") ||
+        (playerPick === "Paper" && computerPickSays === "Rock") ||
+        (playerPick === "Scissors" && computerPickSays === "Paper")
+    ) {
+        result = "You win!";
+        counter.wins += 1;
+    } else {
+        result = "You lose!";
+        counter.loses += 1;
+    }
+
+
+    matchResult.textContent = `Computer picked ${computerPickSays}, you picked ${playerPick}. ${result}`;
+
+    counterSay.textContent = `Wins:${counter.wins} , Loses:${counter.loses} , Ties:${counter.ties} !`;
+}
+
+function deleteScoreFunction() {
+    counter.wins = 0;
+    counter.loses = 0;
+    counter.ties = 0;
+}
+
+// Event listeners pentru butoane
+rockSelected.addEventListener("click", () => playGame("Rock"));
+paperSelected.addEventListener("click", () => playGame("Paper"));
+scissorSelected.addEventListener("click", () => playGame("Scissors"));
+deleteScore.addEventListener("click", () => deleteScoreFunction());
+
+
